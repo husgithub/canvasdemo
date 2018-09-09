2dad41adbada99b8b08afa2691bf54737e5d3499
husong <husong_163mailbox@163.com>
2018-09-10 02:41:02 +0800

    canvas-demo

--- src/main/java/com/hs/itext/test/MyTest.java
+++ src/main/java/com/hs/itext/test/MyTest.java
@@ -4,10 +4,16 @@ import com.hs.itext.util.HTMLTemplateUtils;
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
 import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider;
+import com.itextpdf.kernel.colors.Color;
 import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.canvas.draw.ILineDrawer;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.LineSeparator;
 import com.itextpdf.text.pdf.BaseFont;
 import com.lowagie.text.DocumentException;
 import org.junit.Test;
@@ -94,10 +100,86 @@ public class MyTest {
             PdfDocument pdf = new PdfDocument(writer);
             pdf.setDefaultPageSize(new PageSize(595, 14400));
             Document document = HtmlConverter.convertToDocument(new ByteArrayInputStream(output.getBytes()), pdf, props);
+            // 将所有内容在一个页面显示
+            EndPosition endPosition = new EndPosition();
+            LineSeparator separator = new LineSeparator(endPosition);
+            document.add(separator);
+            document.getRenderer().close();
+            PdfPage page = pdf.getPage(1);
+            float y = endPosition.getY() - 36;
+            page.setMediaBox(new Rectangle(0, y, 595, 14400 - y));
             document.close();
             pdf.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    class EndPosition implements ILineDrawer {
+
+        /** A Y-position. */
+        protected float y;
+
+        /**
+         * Gets the Y-position.
+         *
+         * @return the Y-position
+         */
+        public float getY() {
+            return y;
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * com.itextpdf.kernel.pdf.canvas.draw.ILineDrawer#draw(com.itextpdf.kernel.pdf.
+         * canvas.PdfCanvas, com.itextpdf.kernel.geom.Rectangle)
+         */
+        @Override
+        public void draw(PdfCanvas pdfCanvas, Rectangle rect) {
+            this.y = rect.getY();
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see com.itextpdf.kernel.pdf.canvas.draw.ILineDrawer#getColor()
+         */
+        @Override
+        public Color getColor() {
+            return null;
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see com.itextpdf.kernel.pdf.canvas.draw.ILineDrawer#getLineWidth()
+         */
+        @Override
+        public float getLineWidth() {
+            return 0;
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * com.itextpdf.kernel.pdf.canvas.draw.ILineDrawer#setColor(com.itextpdf.kernel.
+         * color.Color)
+         */
+        @Override
+        public void setColor(Color color) {
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see com.itextpdf.kernel.pdf.canvas.draw.ILineDrawer#setLineWidth(float)
+         */
+        @Override
+        public void setLineWidth(float lineWidth) {
+        }
+
     }
 }
